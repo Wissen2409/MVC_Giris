abeb6f029d3b9504de894a992d03432273a89140
emrahelis <emrahelis@gmail.com>
2024-11-27 20:29:39 +0300

    kdkfdfk

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -13,6 +13,7 @@ public class HomeController : Controller
         _logger = logger;
     }
 
+    
     public IActionResult Index()
     {
         // ekranda gösterilecek olan veriler burada hazırlanır,!!
@@ -24,23 +25,30 @@ public class HomeController : Controller
 
         // Ogrenci isminde hazırlanan modal'in içerisini dolduralım
 
-       GeneralModel model = new GeneralModel();
-        model.Ogrenci = new List<Ogrenci>(){
+        //kontroller kısmında, model kısmında oluşturulan tipin içerisi doldurulup, view 'a gönderildi
+   
+        List<Ogrenci> ogrencis = new List<Ogrenci>(){
 
             new Ogrenci { Name = "Ahmet", Id=1},
             new Ogrenci { Name = "Mehmet", Id=2},
             new Ogrenci{ Name = "Veli", Id=3},
         };
 
+        // action : return view komutu ile , 1. parametre view'in adı
+        // 2. parametre ise hazırlamış olduğu modeli gönderir!!
 
-        return View("Index1",model);
+        //return view ifadesi ise, hazırlanan verileri view'a göndermektir. Aynı zamanda view'i çağırmaktır
+
+        // istek önce controller'a gelir
+        // controller view a döneceği veriyi hazırlar ve sonrasında view'i çağırır!!
+
+        
+        return View("Index1", ogrencis);
     }
-
     public IActionResult Privacy()
     {
         return View();
     }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
--- Models/Ogrenci.cs
+++ Models/Ogrenci.cs
@@ -6,15 +6,4 @@ public class Ogrenci
     public int Id { get; set; }
     public string Name { get; set; }
 }
-public class Ogretmen{
 
-    public int Id { get; set; }
-    public string Name { get; set; }
-}
-
-public class GeneralModel{
-
-    public List<Ogrenci> Ogrenci { get; set; }
-    public List<Ogretmen> Ogretmen{ get; set; }
-
-}
